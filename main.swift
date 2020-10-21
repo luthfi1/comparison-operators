ce8bc38a801d71cb246f471b6f8b8f15fc17aec0
Muhammad Luthfi Farizqi Luthfi <luthfifarizqi23@gmail.com>
2020-10-21 02:26:26 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,20 @@
+//comparison operators
+var score = 190
+var score1 = 90
+print(score >= score1)
+
+
+
+var data = "RPL a"
+var data1 = "Swift"
+print(data <= data1)
+
+
+/*
+<
+>
+==
+<=
+>=
+!=
+*/
